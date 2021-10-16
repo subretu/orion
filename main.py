c5d9bb3c9a5f7ed4209a812e6c087be7456f9b6c
miyahara <50367145+subretu@users.noreply.github.com>
2021-10-16 22:44:36 +0900

    DB接続のポートを環境変数に置き換え

--- main.py
+++ main.py
@@ -66,8 +66,8 @@ def get_connection():
     user = os.getenv('POSTGRES_USER', None)
     pwd = os.getenv('POSTGRES_PASS', None)
     server = os.getenv('POSTGRES_HOST', None)
-    port = "5432"
-    db = os.getenv('POSTGRES_DB', None)
+    port = os.getenv('POSTGRES_HOST', None)
+    db = os.getenv('POSTGRES_PORT', None)
     con = psycopg2.connect("host=" + server + " port=" + port + " dbname=" +
                            db + " user=" + user + " password=" + pwd)
     return con
