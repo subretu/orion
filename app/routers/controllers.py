4c8ea6695369b6f3d58e3fef8eb60a9c8ff99f08
miyahara <50367145+subretu@users.noreply.github.com>
2022-06-18 20:59:02 +0900

    sdkのバージョン指定

--- app/routers/controllers.py
+++ app/routers/controllers.py
@@ -49,7 +49,7 @@ if channel_access_token is None:
     print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
     sys.exit(1)
 
-line_bot_api = AioLineBotApi(channel_access_token)
+line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 
