1161cd4c33d0fa42e1f92c2500f8dc3849fdf037
miyahara <50367145+subretu@users.noreply.github.com>
2022-06-18 15:54:33 +0900

    元に戻す

--- app/routers/controllers.py
+++ app/routers/controllers.py
@@ -34,7 +34,6 @@ from aiolinebot import AioLineBotApi
 from app.database_connection import get_connection
 from app.models.wallet import AggregateWallet, insert_wallet
 from app.models.payer import StorePayer
-from app.models.mode import get_mode, update_mode
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -172,12 +171,6 @@ async def message_text(event):
             )
             StorePayer.pname_id = None
             await line_bot_api.reply_message(event.reply_token, TextSendMessage(text=content))
-        #case "シングルモード":
-        #    update_mode(conn, 1)
-        #    await line_bot_api.reply_message(event.reply_token, TextSendMessage(text="シングルモードに変更します。"))
-        #case "シングルモード解除":
-        #    update_mode(conn, 2)
-        #    await line_bot_api.reply_message(event.reply_token, TextSendMessage(text="シングルモードを解除します。"))
         case _:
             await line_bot_api.reply_message(
                 event.reply_token,
